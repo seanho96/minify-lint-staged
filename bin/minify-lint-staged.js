d32d45a150c9c5ef7849dbdc4ab31997dddebf22
Sean Ho <seanho96@yahoo.com>
2022-01-26 11:08:37 +0800

    fix: module not found error

--- bin/minify-lint-staged.js
+++ bin/minify-lint-staged.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import minifyFile from "../lib/index";
+import minifyFile from "../lib/index.js";
 
 Promise.all(process.argv.slice(2).map(minifyFile)).catch(function (e) {
   console.error(e);
