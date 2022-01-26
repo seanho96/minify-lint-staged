2e80f8086bb2bf9cde42b38ef3e05800c9a09f67
Sean Ho <seanho96@yahoo.com>
2022-01-26 10:37:54 +0800

    fix: change to common JS

--- bin/minify-lint-staged.js
+++ bin/minify-lint-staged.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import minifyFile from "../lib/index.js";
+import minifyFile from "../lib/index.cjs";
 
 Promise.all(process.argv.slice(2).map(minifyFile)).catch(function (e) {
   console.error(e);
