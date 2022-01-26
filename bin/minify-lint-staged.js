4e58fce121063a2cb5432b457bc889e8e0ed90ae
Sean Ho <seanho96@yahoo.com>
2022-01-26 10:30:30 +0800

    fix: change to ES modules import statements

--- bin/minify-lint-staged.js
+++ bin/minify-lint-staged.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const minifyFile = require("../lib/index.js");
+import minifyFile from "../lib/index.js";
 
 Promise.all(process.argv.slice(2).map(minifyFile)).catch(function (e) {
   console.error(e);
