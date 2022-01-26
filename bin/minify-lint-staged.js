f604bba1c1a44070d7506d1e0ee9456d881a1059
Sean Ho <seanho96@yahoo.com>
2022-01-26 11:04:53 +0800

    feat(package.json): add with dependencies

--- bin/minify-lint-staged.js
+++ bin/minify-lint-staged.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import minifyFile from "../lib/index.cjs";
+import minifyFile from "../lib/index";
 
 Promise.all(process.argv.slice(2).map(minifyFile)).catch(function (e) {
   console.error(e);
